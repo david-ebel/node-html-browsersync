7dbed302c8b06cc565105687ebabfa0bd9e437e5
David Ebel <david.ebel@sheknows.com>
2016-04-26 21:29:14 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Starting file for a html node based server. 
+
+Global node module are installed: 
+    - bower
+    - http-server
+    - gulp
+    - browser-sync
+    
+BrowserSync is installed for monitoring html src changes
+    - look at the gulpfile.js
+
+Get Started    
+
+Terminal
+``
+gulp browser-sync
+``
+
+Run Configuration http-server
+
+Preview and select to open in a new tab
---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+
+'use strict';
+
+var gulp = require('gulp'),
+    browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+
+  browserSync({
+    // You can use wildcards in here.
+    files: 'src/index.html, styles.css',
+    // We can pick port 8081 or 8082, if you are more of a 2's kind of guy, go for the 8082. Highly recommended.
+    port: 8082
+  });
+
+});
